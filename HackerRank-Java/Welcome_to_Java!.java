2beffdb9b4140bc207f508f8f3df9e8799ee97e1
Aakanxa Modha <142956130+aakanxamodha@users.noreply.github.com>
2024-02-25 00:08:36 +0530

    Create Welcome_to_Java!.java

---
+++ HackerRank-Java/Welcome_to_Java!.java
@@ -0,0 +1,9 @@
+//Java 7
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
