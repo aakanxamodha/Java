94612c9fa72f005deaf7d25f68bfbbf4c7986110
Aakanxa Modha <142956130+aakanxamodha@users.noreply.github.com>
2024-02-26 00:00:21 +0530

    Create Java_Strings_Introduction.java

---
+++ HackerRank-Java/Java_Strings_Introduction.java
@@ -0,0 +1,32 @@
+//Java 7
+
+/*There are three lines of output:
+For the first line, sum the lengths of A and B.
+For the second line, write Yes if A is lexicographically greater than B otherwise print No instead.
+For the third line, capitalize the first letter in both A and B and print them on a single line, separated by a space.*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        
+        int sum = A.length() + B.length();
+        System.out.println(sum);
+        
+        if(A.compareTo(B) > 0)
+            System.out.println("Yes");   
+        else
+            System.out.println("No");
+            
+        String capA = A.substring(0, 1).toUpperCase() + A.substring(1);
+        String capB = B.substring(0, 1).toUpperCase() + B.substring(1);
+        System.out.println(capA + " " + capB);
+        
+    }
+}
